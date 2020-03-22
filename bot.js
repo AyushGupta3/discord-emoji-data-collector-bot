5b78589fcf55cfeb6e09cfd809cfe9a6a665b169
Ayush Gupta <ayushgupta3@hotmail.com>
2020-03-22 18:32:40 -0400

    - Modify JSON structure to account for sort by count - Add last used field to JSON

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js")
+const moment = require('moment')
 const bot = new Discord.Client()
 const log = console.log
-const token = ''
+const token = 'NjkxMTU2NjU5Mzc2MDk1MjY0.Xnb4FQ.bjd90lDJ0bvWud3rr8EjCaIXC6s'
 const fs = require("fs")
 
 const getData = () => JSON.parse(fs.readFileSync("./emoji_data.json"))
@@ -9,9 +10,22 @@ const updateData = arr => {
     if (! Array.isArray(arr)) return;
     const data = getData()
     arr.map(val => {
-        if (!(val in data)) data[val] = 1;
-        else data[val]++
+        let time = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+        let emoji = data.find(v => v.emoji === val)
+        if (emoji == null){
+            data.push({
+                emoji: val,
+                count: 1,
+                last_used: time
+            })
+            
+        }else{
+            emoji.count++
+            emoji.last_used = time
+        }
+        
     })
+    data.sort((a, b) => b.count - a.count)
     fs.writeFileSync('./emoji_data.json', JSON.stringify(data, null, 4))
 }
 
@@ -19,7 +33,7 @@ const updateData = arr => {
 bot.on("ready", () =>{
     log('Online')
     if(!fs.existsSync("emoji_data.json")){
-        fs.writeFileSync("./emoji_data.json", JSON.stringify({}, null, 4))
+        fs.writeFileSync("./emoji_data.json", JSON.stringify([], null, 4))
     }
 })
 
