4299ec8a0b000292646642a6a3f35cd3150c0a63
Ayush Gupta <ayushgupta3@hotmail.com>
2020-03-22 17:30:33 -0400

    Partially working bot

--- bot.js
+++ bot.js
@@ -5,16 +5,35 @@ const token = ''
 const fs = require("fs")
 
 const getData = () => JSON.parse(fs.readFileSync("./emoji_data.json"))
+const updateData = arr => {
+    if (! Array.isArray(arr)) return;
+    const data = getData()
+    arr.map(val => {
+        if (!(val in data)){
+            data[val] = 1;
+        }
+        else {
+            data[val]++
+        }
+    })
+    fs.writeFileSync('./emoji_data.json', JSON.stringify(data, null, 4))
+}
+
 
 bot.on("ready", () =>{
     log('Online')
     if(!fs.existsSync("emoji_data.json")){
-        fs.writeFileSync("./emoji_data.json", JSON.stringify({}))
+        fs.writeFileSync("./emoji_data.json", JSON.stringify({}, null, 4))
     }
 })
 
 bot.on("message", msg => {
-    
+    const exp = /<:\w+:\d+>/g
+    const matches = msg.content.match(exp)
+    updateData(matches)
+    if (msg.content === "emoji stats"){
+        msg.channel.send(JSON.stringify(getData(), null, 4))
+    }
 })
 
 bot.login(token)
